7c35f3bb96949abddfda5390e28667fd0f30be1f
namenok <comfykh810@gmail.com>
2025-11-04 20:19:45 +0200

    parse works

--- scraper.py
+++ scraper.py
@@ -18,51 +18,60 @@ async def scrape_rozetka():
         path = "https://rozetka.com.ua/"
         await page.goto(path)
 
+
         try:
             await page.click('[data-testid="cookie-banner-close"]', timeout=3000)
-
+            print("closed cocki baner")
         except Exception as e:
-            print("coki ban did not appear, carry  on")
+            print("no coocki baner")
 
         try:
             await page.click('[data-testid="city-confirmation-cancel"]', timeout=2000)
-
+            print("closed town.")
         except Exception as e:
-            print("town did not appear, carry on")
+            print("town did not appear")
 
-        await page.fill('[name="search"]', 'навушники')
+        await page.fill('[name="search"]', 'headphones')
 
         await page.click('[data-testid="search-suggest-submit"]')
 
-
         await page.wait_for_url("**/headphones/**", timeout=60000)
 
-        product_card_selector = "div.catalog-grid__cell"
+
+        product_card_selector = "div.item"
 
         try:
+            print(f"wait till cards will appear on html ({product_card_selector})...")
+
             await page.wait_for_selector(product_card_selector, state='attached', timeout=60000)
+            print("objects card pinned to html")
 
+            # Збираємо дані за цим селектором
             product_cards = await page.locator(product_card_selector).all()
-
+            print(f"Found {len(product_cards)} objects on the page")
 
             for card in product_cards:
                 try:
 
-                    title_element = card.locator(".tile-title")
-                    price_element = card.locator(".rz-tile-price")
+                    title_element = card.locator("a.tile-title")
+                    price_element = card.locator("rz-tile-price")
 
                     title = await title_element.text_content()
                     price = await price_element.text_content()
 
-                    title_clean = title.strip()
-                    price_clean = price.strip().replace('\xa0', ' ')
+                    if title and price:
+                        title_clean = title.strip()
+                        price_clean = price.strip().replace('\xa0', ' ')
 
-                    print(f"НАЗВА: {title_clean} | ЦІНА: {price_clean}")
-                    scraped_data.append({"title": title_clean, "price": price_clean})
+                        print(f"name: {title_clean} | prise: {price_clean}")
+                        scraped_data.append({"title": title_clean, "price": price_clean})
+                    else:
+                        print("card without name, prise- may be advertisement")
+
 
                 except Exception as e:
 
-                    print(f"did not manage to parse")
+                    print(f"didnt manage to parse the card: {e}")
 
         except PlaywrightTimeoutError:
             print("\n" + "=" * 30)
@@ -73,6 +82,8 @@ async def scrape_rozetka():
             with open("debug.html", "w", encoding="utf-8") as f:
                 f.write(page_content)
 
+            print("!!! saved debug.png та debug.html. .")
+            print("=" * 30 + "\n")
 
         await browser.close()
         return scraped_data
@@ -80,5 +91,5 @@ async def scrape_rozetka():
 
 if __name__ == "__main__":
     data = asyncio.run(scrape_rozetka())
-    print("\n--- result ---")
+    print("\n--- РЕЗУЛЬТАТ ---")
     print(data)
